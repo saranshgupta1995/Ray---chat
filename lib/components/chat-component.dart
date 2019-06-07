8d19a572f04923e5552691fe4378111b1fa0170d
Saransh <saranshgupta1995@gmail.com>
2019-06-07 19:54:23 +0530

    basic chat ui

--- lib/components/chat-component.dart
+++ lib/components/chat-component.dart
@@ -1,10 +1,57 @@
 import 'package:flutter/material.dart';
 
 class _ChatWindowState extends State<ChatWindow> {
+  final chatData = [
+    {'n': 'S', 'c': 'Hi!!', 't': 'bot', 'name': 'Saransh'}
+  ];
+
   Widget build(BuildContext context) {
     return Expanded(
-      child: Row(),
-    );
+        child: ListView(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            children: List.generate(chatData.length, (i) {
+              if (chatData[i]['t'] == 'bot')
+                return Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  // elevation: 0,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            // height: 50,
+                            // width: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              // borderRadius: BorderRadius.circular(10)
+                            ),
+                            padding: EdgeInsets.all(10),
+                            child: Text(chatData[i]['n']),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                              child: Text(
+                                chatData[i]['name'],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                              child: Text(chatData[i]['c']),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+            })));
   }
 }
 
