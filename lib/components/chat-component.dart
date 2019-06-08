cc130f4b3a9f26926bf27d9717f2bac331034259
Saransh <saranshgupta1995@gmail.com>
2019-06-08 13:23:02 +0530

    code quality

--- lib/components/chat-component.dart
+++ lib/components/chat-component.dart
@@ -2,56 +2,60 @@ import 'package:flutter/material.dart';
 
 class _ChatWindowState extends State<ChatWindow> {
   final chatData = [
-    {'n': 'S', 'c': 'Hi!!', 't': 'bot', 'name': 'Saransh'}
+    {'n': 'S', 'message': 'Hi!!', 'type': 'bot', 'name': 'Saransh'},
+    {'n': 'S', 'message': 'Hi!!', 'type': 'bot', 'name': 'Saransh'},
   ];
 
   Widget build(BuildContext context) {
     return Expanded(
-        child: ListView(
-            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-            children: List.generate(chatData.length, (i) {
-              if (chatData[i]['t'] == 'bot')
-                return Container(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  // elevation: 0,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      child: ListView(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        children: List.generate(chatData.length, (i) {
+          if (chatData[i]['type'] == 'bot')
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              // elevation: 0,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Column(
                     children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            // height: 50,
-                            // width: 50,
-                            decoration: BoxDecoration(
-                              color: Colors.red,
-                              // borderRadius: BorderRadius.circular(10)
-                            ),
-                            padding: EdgeInsets.all(10),
-                            child: Text(chatData[i]['n']),
-                          ),
-                        ],
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                              child: Text(
-                                chatData[i]['name'],
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                              child: Text(chatData[i]['c']),
-                            ),
-                          ],
+                      Container(
+                        // height: 50,
+                        // width: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          // borderRadius: BorderRadius.circular(10)
                         ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(chatData[i]['n']),
                       ),
                     ],
                   ),
-                );
-            })));
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          child: Text(
+                            chatData[i]['name'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          child: Text(chatData[i]['message']),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+        }),
+      ),
+    );
   }
 }
 
